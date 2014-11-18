0d71e3b8d1b5dd2aa7316303fa903381a1566e06
zengke <kandx@outlook.com>
2014-11-18 14:51:11 +0800

    add models to admin site

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import Department, Duty, Level, Character, Profile
 
 # Register your models here.
+admin.site.register(Department)
+admin.site.register(Duty)
+admin.site.register(Level)
+admin.site.register(Character)
+admin.site.register(Profile)
+
